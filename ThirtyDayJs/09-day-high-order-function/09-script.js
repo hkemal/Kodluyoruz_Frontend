36ffc1a13df081105b67154e111ba0ce5700ed65
hkemal <hasankemal96@gmail.com>
2022-12-30 09:01:05 +0300

    09-day

---
+++ ThirtyDayJs/09-day-high-order-function/09-script.js
@@ -0,0 +1,41 @@
+const func = (number) => number ** 2;
+
+function cube(cb, number) {
+  return cb(number) * number;
+}
+
+//console.log(cube(func, 3));
+
+const a = (s1) => {
+  const b = (s2) => {
+    const c = (s3) => {
+      return s1 + s2 + s3;
+    };
+    return c;
+  };
+  return b;
+};
+
+//console.log(a(5)(10)(15));
+
+const numbers = [1, 3, 5, 7, 9];
+let total = 0;
+numbers.forEach((number) => (total += number));
+console.log(total);
+
+function sayHello() {
+  console.log("Hello");
+}
+const interval = setInterval(sayHello, 1000);
+//clearInterval
+
+function sayWorld() {
+  console.log("World");
+}
+setTimeout(sayWorld, 5000);
+//clearTimeout
+
+setTimeout(() => {
+  clearInterval(interval);
+  console.log("interval iptal edildi");
+}, 5000);
