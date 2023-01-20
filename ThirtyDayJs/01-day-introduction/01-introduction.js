8af5dbbe955bf1a87cf829d736c475a0c5b53dd9
kemal <hasankemal.erkaraca@aurorabilisim.com>
2023-01-20 11:47:42 +0300

    01-day

---
+++ ThirtyDayJs/01-day-introduction/01-introduction.js
@@ -0,0 +1,32 @@
+console.log("Hello, World!");
+console.log("Hello, World!");
+
+console.log(2 + 3); // Addition
+console.log(3 - 2); // Subtraction
+console.log(2 * 3); // Multiplication
+console.log(3 / 2); // Division
+console.log(3 % 2); // Modulus - finding remainder
+console.log(3 ** 2); // Exponentiation 3 ** 2 == 3 * 3
+
+console.log(typeof "Asabeneh"); // string
+console.log(typeof 5); // number
+console.log(typeof true); // boolean
+console.log(typeof null); // object type
+console.log(typeof undefined); // undefined
+
+// Declaring different variables of different data types
+let firstName = "Asabeneh"; // first name of a person
+let lastName = "Yetayeh"; // last name of a person
+let country = "Finland"; // country
+let city = "Helsinki"; // capital city
+let age = 100; // age in years
+let isMarried = true;
+
+console.log(firstName, lastName, country, city, age, isMarried);
+
+// Declaring variables with number values
+age = 100; // age in years
+const gravity = 9.81; // earth gravity  in m/s2
+const boilingPoint = 100; // water boiling point, temperature in °C
+const PI = 3.14; // geometrical constant
+console.log(gravity, boilingPoint, PI);
