524ddbd1c91cb76e406ffcdb2ad11f372cba04f4
kemal <hasankemal.erkaraca@aurorabilisim.com>
2023-02-03 14:08:51 +0300

    01-06-reactjs-es6

--- ReactJS/index.js
+++ ReactJS/index.js
@@ -1,3 +1,13 @@
-let slugify = require("slugify");
-const title = slugify("some string");
-console.log(title);
+// import slugify from "slugify";
+
+// const title = slugify("some string");
+
+// console.log(title);
+
+import hello, { cikar, numArr, topla, user } from "./my-module.js";
+
+console.log(topla(2, 1));
+console.log(cikar(2, 1));
+console.log(hello("kemal"));
+console.log(user);
+console.log(numArr);
---
+++ ReactJS/my-module.js
@@ -0,0 +1,18 @@
+export default (name) => {
+  console.log(`hello  ${name}`);
+};
+
+const topla = (a, b) => a + b;
+
+const cikar = (a, b) => a - b;
+
+const text = `Text`;
+
+const user = {
+  name: "John",
+  surname: "Doe",
+};
+
+const numArr = [1, 2, 3, 4];
+
+export { topla, cikar, text, user, numArr };
