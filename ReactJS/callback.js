ed5c71192f646a2eac7b5c43d7412aa3ddcf6e3d
kemal <hasankemal.erkaraca@aurorabilisim.com>
2023-02-04 23:50:52 +0300

    01-08-reactjs-promises

--- ReactJS/callback.js
+++ ReactJS/callback.js
@@ -1,3 +1,4 @@
+const axios = require('axios');
 // setTimeout(() => {
 //   console.log("Merhaba");
 // }, 5000);
@@ -28,15 +29,70 @@
 //     console.log(responseJson);
 //   });
 
-import fetch from 'node-fetch';
+// import fetch from 'node-fetch';
 
-async function getData() {
-  try {
-    const data = await fetch('https://jsonplaceholder.typicode.com/users');
-    console.log(data);
-  } catch (err) {
-    console.log(err);
-  }
-}
+// async function getData() {
+//   try {
+//     const data = await fetch('https://jsonplaceholder.typicode.com/users');
+//     console.log(data);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
 
-getData();
+// getData();
+
+// console.log('Merhaba');
+// (async () => {
+//   const { data: users } = await axios(
+//     'https://jsonplaceholder.typicode.com/users'
+//   );
+
+//   const { data: post1 } = await axios(
+//     'https://jsonplaceholder.typicode.com/posts/1'
+//   );
+
+//   const { data: post2 } = await axios(
+//     'https://jsonplaceholder.typicode.com/posts/2'
+//   );
+
+//   console.log('users', users);
+//   console.log('***********');
+//   console.log('users', post1);
+//   console.log('***********');
+//   console.log('users', post2);
+// })();
+
+const getUsers = (number) => {
+  return new Promise(async (resolve, reject) => {
+    const { data } = await axios('https://jsonplaceholder.typicode.com/users');
+    resolve(data);
+  });
+};
+
+const getPost = (post_id) => {
+  return new Promise(async (resolve, reject) => {
+    const { data } = await axios(
+      'https://jsonplaceholder.typicode.com/posts/' + post_id
+    );
+    resolve(data);
+  });
+};
+
+// (async () => {
+//   try {
+//     const users = await getUsers();
+//     const post1 = await getPost(1);
+//     const post2 = await getPost(2);
+
+//     console.log(users);
+//     console.log(post1);
+//     console.log(post2);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// })();
+
+Promise.all([getUsers(), getPost(1), getPost(2)])
+  .then(console.log)
+  .catch(console.log);
