79264ecc4eae6a7eb749a9dd77a36018a2b6742e
kemal <hasankemal.erkaraca@aurorabilisim.com>
2023-02-06 11:18:34 +0300

    02-04-ReactJs-jsx

--- ReactJS/02-Components/hello-react/src/App.js
+++ ReactJS/02-Components/hello-react/src/App.js
@@ -1,13 +1,22 @@
+import React from "react";
 import "./App.css";
 import Header from "./components/Header";
 
 function App() {
+  // return React.createElement("div", null, "Hello");
   return (
+    //Kapsayıcı bir etiket olması lazım, boş bir etiket de kullanabiliriz <> </>
     <div>
       <h1>Hello React</h1>
       <Header />
-      <Header />
-      <Header />
+      <p className="xyz">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet ipsum
+        officiis quidem sit aliquam id saepe vero, ea quasi sint quo excepturi
+        obcaecati, optio in nisi adipisci incidunt reprehenderit modi!
+      </p>
+      <label htmlFor="myinput">
+        Name <input id="myinput" />
+      </label>
     </div>
   );
 }
