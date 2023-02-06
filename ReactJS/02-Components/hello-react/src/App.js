75b3a72146dd1676a88f9f41002dedde71ecf173
kemal <hasankemal.erkaraca@aurorabilisim.com>
2023-02-06 11:22:29 +0300

    02-05-ReactJs-render-variable-in-components

--- ReactJS/02-Components/hello-react/src/App.js
+++ ReactJS/02-Components/hello-react/src/App.js
@@ -2,21 +2,27 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header";
 
+const name = "Mehmet";
+const surname = "Seven";
+
 function App() {
   // return React.createElement("div", null, "Hello");
   return (
     //Kapsayıcı bir etiket olması lazım, boş bir etiket de kullanabiliriz <> </>
+    // <div>
+    //   <h1>Hello React</h1>
+    //   <Header />
+    //   <p className="xyz">
+    //     Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet ipsum
+    //     officiis quidem sit aliquam id saepe vero, ea quasi sint quo excepturi
+    //     obcaecati, optio in nisi adipisci incidunt reprehenderit modi!
+    //   </p>
+    //   <label htmlFor="myinput">
+    //     Name <input id="myinput" />
+    //   </label>
+    // </div>
     <div>
-      <h1>Hello React</h1>
-      <Header />
-      <p className="xyz">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet ipsum
-        officiis quidem sit aliquam id saepe vero, ea quasi sint quo excepturi
-        obcaecati, optio in nisi adipisci incidunt reprehenderit modi!
-      </p>
-      <label htmlFor="myinput">
-        Name <input id="myinput" />
-      </label>
+      <h1>{`Benim adim ${name} soyadim ${surname}`}</h1>
     </div>
   );
 }
