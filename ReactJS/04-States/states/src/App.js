009b0f3b9ddec2de8e9229d166569dc6950fec89
hkemal <hasankemal96@gmail.com>
2023-02-08 23:44:09 +0300

    04-02-ReactJs-array-states

--- ReactJS/04-States/states/src/App.js
+++ ReactJS/04-States/states/src/App.js
@@ -5,12 +5,23 @@ import "./App.css";
 function App() {
   const [name, setName] = useState("Kemal");
   const [age, setAge] = useState(29);
+  const [friends, setFriends] = useState(["Ahmet", "Murat"]);
   return (
     <div className="App">
       <h1>Merhaba {name}</h1>
       <h2>{age}</h2>
       <button onClick={() => setName("Hasan")}>Change Name</button>
       <button onClick={() => setAge(26)}>Change Age</button>
+
+      <hr />
+      <br />
+      <h2>Friends</h2>
+      {friends.map((friend, index) => (
+        <div key={index}>{friend}</div>
+      ))}
+      <button onClick={() => setFriends([...friends, "Ayşe"])}>
+        Add New Friend
+      </button>
     </div>
   );
 }
