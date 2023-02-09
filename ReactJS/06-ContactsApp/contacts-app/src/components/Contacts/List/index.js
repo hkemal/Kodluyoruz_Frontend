3bfbd5b2fffe39ea37792cf4569080fb7b38598d
kemal <hasankemal.erkaraca@aurorabilisim.com>
2023-02-09 19:31:05 +0300

    06-04-ReactJs-filter

--- ReactJS/06-ContactsApp/contacts-app/src/components/Contacts/List/index.js
+++ ReactJS/06-ContactsApp/contacts-app/src/components/Contacts/List/index.js
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 function List({ contacts }) {
+  const [filterText, setFilterText] = useState("");
+
+  const filtered = contacts.filter((item) => {
+    return Object.keys(item).some((key) => {
+      return item[key]
+        .toString()
+        .toLowerCase()
+        .includes(filterText.toLocaleLowerCase());
+    });
+  });
+
+  console.log(filtered);
+
   return (
     <div>
+      <input
+        placeholder="Filter contact"
+        value={filterText}
+        onChange={(e) => setFilterText(e.target.value)}
+      />
       <ul>
         {contacts.map((contact, i) => (
           <li key={i}>{contact.fullname}</li>
--- ReactJS/06-ContactsApp/contacts-app/src/components/Contacts/index.js
+++ ReactJS/06-ContactsApp/contacts-app/src/components/Contacts/index.js
@@ -3,7 +3,20 @@ import Form from "./Form";
 import List from "./List";
 
 function Contacts() {
-  const [contacts, setContacts] = useState([]);
+  const [contacts, setContacts] = useState([
+    {
+      fullname: "Mehmet",
+      phone_number: "123123",
+    },
+    {
+      fullname: "Kemal",
+      phone_number: "456554",
+    },
+    {
+      fullname: "Nazlı",
+      phone_number: "78897878",
+    },
+  ]);
 
   useEffect(() => {
     console.log(contacts);
