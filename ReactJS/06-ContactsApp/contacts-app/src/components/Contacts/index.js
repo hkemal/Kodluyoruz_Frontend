677195e2d825bd250687daa6d16aad1370dd0f1f
kemal <hasankemal.erkaraca@aurorabilisim.com>
2023-02-09 17:01:28 +0300

    06-02-ReactJs--form-component-development

--- ReactJS/06-ContactsApp/contacts-app/src/components/Contacts/index.js
+++ ReactJS/06-ContactsApp/contacts-app/src/components/Contacts/index.js
@@ -1,12 +1,18 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import Form from "./Form";
 import List from "./List";
 
 function Contacts() {
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    console.log(contacts);
+  }, [contacts]);
+
   return (
     <div>
       <List />
-      <Form />
+      <Form contacts={contacts} addContact={setContacts} />
     </div>
   );
 }
