4af432db133f2b764ba7d8abfee3e1655da139ef
kemal <hasankemal.erkaraca@aurorabilisim.com>
2023-02-09 17:08:16 +0300

    06-03-ReactJs-list-component-development

--- ReactJS/06-ContactsApp/contacts-app/src/components/Contacts/List/index.js
+++ ReactJS/06-ContactsApp/contacts-app/src/components/Contacts/List/index.js
@@ -1,7 +1,15 @@
 import React from "react";
 
-function List() {
-  return <div>List</div>;
+function List({ contacts }) {
+  return (
+    <div>
+      <ul>
+        {contacts.map((contact, i) => (
+          <li key={i}>{contact.fullname}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default List;
