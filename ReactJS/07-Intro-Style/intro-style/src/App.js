ccbb59cf7777b4366873475908944bf90a977856
kemal <hasankemal.erkaraca@aurorabilisim.com>
2023-02-12 22:52:06 +0300

    07-02-ReactJs-module-css

--- ReactJS/07-Intro-Style/intro-style/src/App.js
+++ ReactJS/07-Intro-Style/intro-style/src/App.js
@@ -1,20 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
+import A from "./components/A";
+import B from "./components/B";
 
 function App() {
   return (
     <div className="App">
-      <div
-        style={{ color: "red", backgroundColor: "yellow", paddingTop: "10px" }}
-      >
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae quia qui
-        enim ipsam dicta accusantium neque assumenda eveniet iste ad architecto
-        veniam ipsa laborum voluptatibus adipisci quo, repudiandae inventore
-        odio?
-      </div>
-      <div class="alert alert-primary" role="alert">
-        This is a primary alert—check it out!
-      </div>
+      <header className="App-header">
+        <A />
+        <B />
+      </header>
     </div>
   );
 }
