5811bdc7300c0d705f4bd7f0a824001975395a6f
kemal <hasankemal.erkaraca@aurorabilisim.com>
2023-02-15 06:44:49 +0300

    11-02-ReactJs-useMemo

--- ReactJS/11-React-Memo/react-memo/src/App.js
+++ ReactJS/11-React-Memo/react-memo/src/App.js
@@ -1,17 +1,35 @@
-import { useState } from "react";
+import { useState, useMemo } from "react";
 import "./App.css";
 import Header from "./components/Header";
 
 function App() {
   const [number, setNumber] = useState(0);
+  const [text, setText] = useState("");
+
+  const data = useMemo(() => {
+    return calculateObject();
+  }, []);
+
+  // const data = calculateObject();
 
   return (
     <div className="App">
-      <Header number={number < 5 ? 0 : number} />
+      <Header number={number} data={data} />
       <h1>{number}</h1>
       <button onClick={() => setNumber(number + 1)}>Click</button>
+
+      <br />
+      <br />
+      <input value={text} onChange={({ target }) => setText(target.value)} />
     </div>
   );
+}
+
+function calculateObject() {
+  console.log("Calculating...");
+  for (let i = 0; i < 1000000000; i++) {}
+  console.log("Calculating complated!");
+  return { name: "John" };
 }
 
 export default App;
--- ReactJS/11-React-Memo/react-memo/src/components/Header.js
+++ ReactJS/11-React-Memo/react-memo/src/components/Header.js
@@ -1,8 +1,16 @@
 import React from "react";
 
-function Header({ number }) {
+function Header({ number, data }) {
   console.log("Header component re-rendered.");
-  return <div>Header - {number}</div>;
+  return (
+    <div>
+      Header - {number}
+      <br />
+      <br />
+      <code>{JSON.stringify(data)}</code>
+      <hr />
+    </div>
+  );
 }
 
 export default React.memo(Header);
