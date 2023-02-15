fa3129796cedbd7b618497e9eb729371cca0cdf4
kemal <hasankemal.erkaraca@aurorabilisim.com>
2023-02-15 06:25:15 +0300

    11-01-ReactJs-React.memo

---
+++ ReactJS/11-React-Memo/react-memo/src/components/Header.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Header({ number }) {
+  console.log("Header component re-rendered.");
+  return <div>Header - {number}</div>;
+}
+
+export default React.memo(Header);
