3be65f429d1305926b2d1f5740cca2176f6d5703
kemal <hasankemal.erkaraca@aurorabilisim.com>
2023-02-20 13:46:55 +0300

    13-03-ReactJs-transfer-data-backend

--- ReactJS/13-Sample-Realtime-App/client/src/components/Palette.js
+++ ReactJS/13-Sample-Realtime-App/client/src/components/Palette.js
@@ -1,11 +1,18 @@
-import React from "react";
+import { useState } from "react";
 import "../App.css";
+import { send } from "../socketApi";
 
 function Palette() {
+  const [color, setColor] = useState("#000");
+
   return (
     <div className="palette">
-      <input type="color" />
-      <button>Click</button>
+      <input
+        type="color"
+        value={color}
+        onChange={(e) => setColor(e.target.value)}
+      />
+      <button onClick={() => send(color)}>Click</button>
     </div>
   );
 }
