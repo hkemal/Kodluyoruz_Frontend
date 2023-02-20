3d3267515dd0daeb065751326161bcc58b889f3a
kemal <hasankemal.erkaraca@aurorabilisim.com>
2023-02-20 13:36:48 +0300

    13-02-ReactJs-connect-to-socket-server

--- ReactJS/13-Sample-Realtime-App/client/src/App.js
+++ ReactJS/13-Sample-Realtime-App/client/src/App.js
@@ -1,7 +1,12 @@
+import { useEffect } from "react";
 import "./App.css";
 import Palette from "./components/Palette";
+import { init } from "./socketApi";
 
 function App() {
+  useEffect(() => {
+    init();
+  }, []);
   return (
     <div className="App">
       <Palette />
